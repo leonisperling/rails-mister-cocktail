d0ccade32860ddb8b4da93974956101488905a74
Leoni Sperling <leoni.sperling@gmail.com>
2020-02-20 18:10:21 +0100

    add destroy method

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,12 +1,14 @@
 class DosesController < ApplicationController
-  before_action :set_cocktail, only: [:new, :create]
+  before_action :set_dose, only: [:show, :edit]
 
   def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
 
   def create
     @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail)
@@ -16,17 +18,21 @@ class DosesController < ApplicationController
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to cocktails_path
+    respond_to do |format|
+      format.html { redirect_to cocktail_path(@dose.cocktail), notice: 'dose was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
 
-  def set_cocktail
-    @cocktail = Cocktail.find(params[:cocktail_id])
+  def set_dose
+    @dose = Dose.find(params[:dose_id])
   end
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
   end
 end
