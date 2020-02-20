d1750716de74b8d83c1197e260094825fde4b795
Leoni Sperling <leoni.sperling@gmail.com>
2020-02-20 23:46:15 +0100

    reworked doses controller

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,14 +1,12 @@
 class DosesController < ApplicationController
-  before_action :set_dose, only: [:show, :edit]
+  before_action :set_cocktail, only: [:new, :create]
 
   def new
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
 
   def create
     @dose = Dose.new(dose_params)
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail)
@@ -20,16 +18,13 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
-    respond_to do |format|
-      format.html { redirect_to cocktail_path(@dose.cocktail), notice: 'dose was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to cocktail_path(@dose.cocktail), notice: 'dose was successfully destroyed.'
   end
 
   private
 
-  def set_dose
-    @dose = Dose.find(params[:dose_id])
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
   def dose_params
